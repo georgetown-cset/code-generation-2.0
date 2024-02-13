ee82c41b5e8987cb5d107b8e3205dc9d19c4514d
mwu2000 <mw1442@georgetown.edu>
2024-02-13 10:37:09 -0500

    refactoring

--- scripts/esbmc_call.py
+++ scripts/esbmc_call.py
@@ -4,7 +4,7 @@ import os
 CURRENT_DIRECTORY = os.getcwd()
 
 #target directory
-dest_path = os.path.join(CURRENT_DIRECTORY, "esbmc_output")
+dest_path = os.path.join(CURRENT_DIRECTORY, "wizard_esbmc")
 
   
 # Iterate through generated code files
@@ -24,13 +24,13 @@ dest_path = os.path.join(CURRENT_DIRECTORY, "esbmc_output")
 #         with open(target_file_name, "w") as file:
 #             file.write(esbmc_result.stderr)
 
-def esbmc_run(code_snippet):
+def esbmc_run(code_snippet, folder_name):
     #target directory
-    dest_path = os.path.join(CURRENT_DIRECTORY, "esbmc_output")
-    if os.path.isfile(os.path.join(CURRENT_DIRECTORY, "generated_code", code_snippet)):
+    dest_path = os.path.join(CURRENT_DIRECTORY, "wizard_esbmc")
+    if os.path.isfile(os.path.join(CURRENT_DIRECTORY, folder_name, code_snippet)):
         print(code_snippet)
         esbmc_result = subprocess.run(
-                ["esbmc", os.path.join(CURRENT_DIRECTORY, "generated_code", code_snippet),
+                ["esbmc", os.path.join(CURRENT_DIRECTORY, folder_name, code_snippet),
                 "--k-induction-parallel", "--overflow-check", "--memory-leak-check",
                 "--multi-property", "--timeout", "1900s"], capture_output= True, text = True)
         print(esbmc_result.stderr)
@@ -42,20 +42,24 @@ def esbmc_run(code_snippet):
 
 if __name__ == "__main__":
     
-    target_files = [
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-119_BOF-1a.c"),
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-79_INI-1c.c"),
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-476_NPD-2a.c"),
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-416_UAF-1a.c"),
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-476_NPD-2b.c"),
-    os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-79_INI-1b.c")]
+    # target_files = [
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-119_BOF-1a.c"),
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-79_INI-1c.c"),
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-476_NPD-2a.c"),
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-416_UAF-1a.c"),
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-476_NPD-2b.c"),
+    # os.path.join(CURRENT_DIRECTORY, "generated_code", "CWE-79_INI-1b.c")]
 
     
-    for code_snippet in target_files:
-        esbmc_run(code_snippet)
+    # for code_snippet in target_files:
+    #     esbmc_run(code_snippet)
   
     # for code_snippet in os.listdir(os.path.join(CURRENT_DIRECTORY, "generated_code")):
     #     esbmc_run(code_snippet)
+    
+    for code_snippet in os.listdir(os.path.join(CURRENT_DIRECTORY, "generated_wizard_coder")):
+        esbmc_run(code_snippet, "generated_wizard_coder")
+
 
 
 
